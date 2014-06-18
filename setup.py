4ed3eb3ee77d26026b4f49725db06d488461d9f8
Pyotr Ermishkin <quasiyoke@gmail.com>
2014-06-19 02:12:30 +0400

    "init" command is available at setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@
 import ez_setup
 ez_setup.use_setuptools()
 
+import logging
 import setuptools
+import sys
+
+logging.basicConfig()
 
 
 class create_tables(setuptools.Command):
@@ -20,6 +24,32 @@ class create_tables(setuptools.Command):
     def run(self):
         from vk_relations import models
         models.create_tables()
+
+
+class init(setuptools.Command):
+    description = 'initialize DB with first persons'
+
+    user_options = [
+        ('user-id=', 'u', 'first person which friends will be used for initialization'),
+        ('count=', 'c', 'how much users should we retrieve'),
+    ]
+
+    def initialize_options(self):
+        self.user_id = None
+        self.count = None
+
+    def finalize_options(self):
+        if self.user_id is None:
+            logging.getLogger(__name__).critical('"user-id" parameter wasn\'t specified.')
+            sys.exit()
+        if self.count is None:
+            logging.getLogger(__name__).critical('"count" parameter wasn\'t specified.')
+            sys.exit()
+        self.count = int(self.count)
+
+    def run(self):
+        from vk_relations import main
+        main.init(self.user_id, self.count)
         
 
 setuptools.setup(
@@ -35,8 +65,11 @@ setuptools.setup(
     install_requires=[
         'MySQLdb',
         'peewee',
+        'requests',
+        'vk_api',
     ],
     cmdclass={
         'create_tables': create_tables,
+        'init': init,
     }
 )
