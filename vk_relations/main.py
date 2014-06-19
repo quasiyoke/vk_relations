a99e11a990fce722ab0f7065eda9208e55641f81
Pyotr Ermishkin <quasiyoke@gmail.com>
2014-06-20 01:49:40 +0400

    Constraint problem with persons with relations was solved.

--- vk_relations/main.py
+++ vk_relations/main.py
@@ -7,18 +7,39 @@ import vk
 def init(parent, count):
     now = datetime.datetime.now()
     i = 0
+    retrieved_persons_ids = set()
     for person_data in vk.get_persons(parent, count):
         person_kwargs = {
             'id': person_data['id'],
             'sex': person_data['sex'],
-            'check_date': now
+            'check_date': now,
         }
         try:
-            person_kwargs['relation']=person_data['relation']
-            person_kwargs['relation_partner']=person_data['relation_partner']['id']
+            person_kwargs['relation'] = person_data['relation']
+            person_kwargs['relation_partner'] = person_data['relation_partner']['id']
         except KeyError:
             pass
-        person = models.Person.create(**person_kwargs)
+        else:
+            # If we have no relation partner in DB, we should create stub of him to prevent constraint-related problems.
+            if person_kwargs['relation_partner'] not in retrieved_persons_ids:
+                models.Person.create(
+                    id=person_kwargs['relation_partner'],
+                    sex='male',
+                    check_date=now,
+                )
+                retrieved_persons_ids.add(person_kwargs['relation_partner'])
+
+        # Person creation.
+        
+        # If we already created this person as someone's relation partner, just update his stub with actual data.
+        if person_kwargs['id'] in retrieved_persons_ids:
+            person = models.Person.select().where(models.Person.id == person_kwargs['id'])[0]
+            for key, value in person_kwargs.items():
+                setattr(person, key, value)
+            person.save()
+        else:
+            person = models.Person.create(**person_kwargs)
+        retrieved_persons_ids.add(person_data['id'])
         i += 1
     print '%d persons saved' % i
     print '%f sec/person' % (float((datetime.datetime.now() - now).seconds) / i)
