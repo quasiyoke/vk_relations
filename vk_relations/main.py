b96caddcbb2838ea1db4ab8ef99a7de1680c2256
Pyotr Ermishkin <quasiyoke@gmail.com>
2014-06-20 00:28:25 +0400

    UPDATEs to DB was replaced with INSERTs.

--- vk_relations/main.py
+++ vk_relations/main.py
@@ -8,18 +8,17 @@ def init(parent, count):
     now = datetime.datetime.now()
     i = 0
     for person_data in vk.get_persons(parent, count):
-        person = models.Person(
-            id=person_data['id'],
-            sex=person_data['sex'],
-            check_date=now,
-        )
+        person_kwargs = {
+            'id': person_data['id'],
+            'sex': person_data['sex'],
+            'check_date': now
+        }
         try:
-            person.relation=person_data['relation']
-            person.relation_partner=person_data['relation_partner']['id']
+            person_kwargs['relation']=person_data['relation']
+            person_kwargs['relation_partner']=person_data['relation_partner']['id']
         except KeyError:
             pass
-        print person.__dict__
-        person.save()
+        person = models.Person.create(**person_kwargs)
         i += 1
     print '%d persons saved' % i
     print '%f sec/person' % (float((datetime.datetime.now() - now).seconds) / i)
