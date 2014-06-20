ab8c80766cf0320ab51ef54993620b192803d907
Pyotr Ermishkin <quasiyoke@gmail.com>
2014-06-20 05:47:45 +0400

    Redundant exceptions catches were removed from models.py

--- vk_relations/models.py
+++ vk_relations/models.py
@@ -56,7 +56,7 @@ def create_tables():
     try:
         Person.create_table()
         RelationChange.create_table()
-    except (peewee.DatabaseError, peewee.OperationalError), e:
+    except peewee.DatabaseError, e:
         logging.getLogger(__name__).critical(e)
         sys.exit()
 
@@ -64,14 +64,14 @@ def create_tables():
 def drop_tables():
     try:
         RelationChange.drop_table()
-    except (peewee.DatabaseError, peewee.OperationalError), e:
+    except peewee.DatabaseError, e:
         e = unicode(e)
         if not e.startswith('(1051'): # Unknown table
             logging.getLogger(__name__).critical(e)
             sys.exit()
     try:
         Person.drop_table()
-    except (peewee.DatabaseError, peewee.OperationalError), e:
+    except peewee.DatabaseError, e:
         e = unicode(e)
         if not e.startswith('(1051'): # Unknown table
             logging.getLogger(__name__).critical(e)
